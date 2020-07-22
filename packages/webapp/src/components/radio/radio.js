3d27299deb878bec05cdcbebe32338c7624882d6
johncoffee <github2020@studio42.dk>
2020-07-22 18:01:29 +0200

    enable events

--- packages/webapp/src/components/radio/radio.js
+++ packages/webapp/src/components/radio/radio.js
@@ -10,13 +10,24 @@ const myTemplate = ({ title, artist, mp3 }) => html`
 <div class="margin-vertical-1">
   
 <!--    <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/640462086&color=0e141b"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/320colab" title="320colab" target="_blank" style="color: #cccccc; text-decoration: none;">320colab</a> · <a href="https://soundcloud.com/320colab/320-open-jam-june-15th" title="320 Open Jam June 15th" target="_blank" style="color: #cccccc; text-decoration: none;">320 Open Jam June 15th</a></div>-->
-  <audio controls style="vertical-align: middle">
+  <audio controls style="vertical-align: middle" @pause="${onPause}" @timeupdate="${ontimeupdate}">
       <source type="audio/mp3" src="${mp3}">
   </audio>
   <button class="button primary margin-0" style="vertical-align: middle" >NEXT</button>
 
 </div>
 `
+
+function onPause (evt) {
+  // console.log(evt)
+  if (evt.target.duration-evt.target.currentTime < 1) {
+    console.log('next!')
+  }
+}
+
+function ontimeupdate (evt) {
+  // console.log(evt)
+}
 
 export function connect (store) {
   store.subscribe(s => {
@@ -31,6 +42,8 @@ export function connect (store) {
     }
   })
 }
+
+// super high level functions
 
 export function play () {
   setTrack({
