ff6360d691b73b170c13ab2744a6c60d1535e53f
johncoffee <github2020@studio42.dk>
2020-07-22 23:13:16 +0200

    skipped track can be logged

--- packages/webapp/src/components/radio/radio.js
+++ packages/webapp/src/components/radio/radio.js
@@ -14,7 +14,7 @@ const myTemplate = ({ title, artist, mp3, index, playlist, skip, onPause, ontime
   
 <!--    <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/640462086&color=0e141b"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/320colab" title="320colab" target="_blank" style="color: #cccccc; text-decoration: none;">320colab</a> · <a href="https://soundcloud.com/320colab/320-open-jam-june-15th" title="320 Open Jam June 15th" target="_blank" style="color: #cccccc; text-decoration: none;">320 Open Jam June 15th</a></div>-->
   <audio controls style="vertical-align: middle" @pause="${onPause}" src="${mp3}" preload="auto"></audio>
-  <button class="button primary margin-0" style="vertical-align: middle" @click="${skip}">NEXT</button>
+  <button class="button primary margin-0" style="vertical-align: middle" @click="${()=>skip(mp3)}">MEH..</button>
 
 </div>
 `
@@ -23,7 +23,8 @@ const myTemplate = ({ title, artist, mp3, index, playlist, skip, onPause, ontime
 export function connect (store) {
 
   store.subscribe(s => {
-    const skip = () => {
+    const skip = (skippedTrack) => {
+      console.log('meh',skippedTrack)
       const nextIndex = (s.playlist.index+1 >= s.playlist.playlist.length) ? 0 : s.playlist.index+1;
       setTrack(s.playlist.playlist[nextIndex])
       skipTrack(nextIndex)
